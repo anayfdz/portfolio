983363a9e4c6baca86f6ced836f29b0975abd29b
Ana <anayfernandez95@gmail.com>
2024-07-21 22:18:25 -0500

    aboutSection

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -2,6 +2,8 @@ import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import AchievementsSection from "./components/AchievementsSection";
+import AboutSection from "./components/AboutSection";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
@@ -9,6 +11,7 @@ export default function Home() {
     <div class="container mt-24 mx-auto px-12 py-4">
     <HeroSection/>
     <AchievementsSection/>
+    <AboutSection/>
     </div>
     </main>
   );
